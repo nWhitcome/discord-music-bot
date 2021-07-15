881b3b2d75462c10fd43e3bd2db09e589c83a207
Dave <contact@davidwhitcome.com>
2021-07-14 21:41:00 -0500

    Make the date, time, and other variables configurable (#9)
    * configure hour for poll and meeting
    
    * customize with minute and day
    
    * config file
    
    * warning comment
    
    * cleanup
    
    * announcementChannel & bugfixes
    
    * optional env variables
    
    * poll day
    
    * remove unused imports
    
    * __pycache__/config.cpython-39.pyc to gitignore
    
    * remove quotes from channel ids
    
    * __pycache__/

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+import calendar
+from dotenv import load_dotenv
+
+# REQUIRES RESTART OF BOT TO APPLY CHANGES
+
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+suggChannel = os.getenv('SUGG_CHANNEL') or 839962435044900874
+announcementChannel = os.getenv('ANNOUNCEMENT_CHANNEL') or 839961783498571867
+testChannel = os.getenv('TEST_CHANNEL') or 840042019971661825
+commandPrefix = '$'
+
+# Meeting date & time
+pollDay = os.getenv('POLL_DAY') or calendar.SUNDAY
+pollHour = os.getenv('POLL_HOUR') or 20
+pollMinute = os.getenv('POLL_MINUTE') or 0
+meetingDay = os.getenv('MEETING_DAY') or calendar.MONDAY
+meetingHour = os.getenv('MEETING_HOUR') or 20
+meetingMinute = os.getenv('MEETING_MINUTE') or 0
--- musicBot.py
+++ musicBot.py
@@ -1,32 +1,33 @@
-import os
-import discord
-from dotenv import load_dotenv
 from discord.ext.commands import Bot
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from apscheduler.triggers.cron import CronTrigger
-from discord.ext import commands
 import sqlite3
 import random
 import calendar
 import datetime
+import config
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-suggChannel = '839962435044900874'
-testChannel = '840042019971661825'
-
-bot = Bot(command_prefix='$')
+bot = Bot(command_prefix=config.commandPrefix)
 
 bot.runOnceFlag = 0
 dictionary = {}
 
-# Gets the last Monday of the month
-def getLastMonday():
+# Gets the last meeting day of the month
+def getLastMeetingDay():
     now = datetime.datetime.now()
-
-    last_monday = max(week[calendar.MONDAY]
+    last_day = max(week[config.meetingDay]
         for week in calendar.monthcalendar(now.year, now.month))
-    return int(last_monday)
+    return int(last_day)
+
+def hourToPrintStandardTime(hour, minute):
+    printableHour = hour
+    printableAmPm = 'AM'
+    if (hour <= 0 or hour >= 24):
+        printableHour = 12
+    elif (hour >= 13):
+        printableHour = hour - 12
+    if (hour >= 11 and hour < 24):
+        printableAmPm = 'PM'
+    return str(printableHour) + ':' + str(minute).zfill(2) + ' ' + str(printableAmPm) + ' CST'
 
 def runOnce():
     if(bot.runOnceFlag == 0):
@@ -43,17 +44,18 @@ def runOnce():
             dictionary[str(row[0])] = str(row[1])
 
         scheduler = AsyncIOScheduler()
-        scheduler.add_job(sendPoll, 'cron', day_of_week='sun', hour=20)
-        scheduler.add_job(chooseWinner, 'cron', day_of_week='mon', hour=20)
+        scheduler.add_job(sendPoll, 'cron', day_of_week=config.pollDay, hour=config.pollHour, minute=config.pollMinute)
+        scheduler.add_job(chooseWinner, 'cron', day_of_week='tue', hour=config.meetingHour, minute=config.meetingMinute)
         scheduler.start()
-
+        print("Poll set for " + calendar.day_name[config.pollDay] + " " + hourToPrintStandardTime(config.pollHour, config.pollMinute))
+        print("Meeting set for " + calendar.day_name[config.meetingDay] + " " + hourToPrintStandardTime(config.meetingHour, config.meetingMinute))
         bot.runOnceFlag = 1
 
 # Sends out a poll so people can vote on album of the week
 async def sendPoll():
-    if(int(datetime.datetime.now().day) + 1 != getLastMonday()):
+    if(int(datetime.datetime.now().day) + 1 != getLastMeetingDay()):
         print("Posting poll...")
-        channel = bot.get_channel(839961783498571867)
+        channel = bot.get_channel(config.announcementChannel)
         pollString = '/poll "<@&839958672868245504>, here is the poll for the album of the week:"'
         for i, j in dictionary.items():
             pollString += f' "{i} - {j}"'
@@ -74,7 +76,7 @@ async def sendPoll():
 @bot.command(name='suggest')
 async def suggest(ctx, *, arg):
     print("Received suggestion")
-    if(str(ctx.channel.id) == suggChannel or str(ctx.channel.id) == testChannel):
+    if(str(ctx.channel.id) == config.suggChannel or str(ctx.channel.id) == config.testChannel):
         dictionary[str(ctx.author)] = str(arg)
         conn = sqlite3.connect('weeklyData.db')
         c = conn.cursor()
@@ -87,7 +89,7 @@ async def suggest(ctx, *, arg):
 async def listSuggestions(ctx):
     print("Listing suggestions")
     listString = ""
-    if(str(ctx.channel.id) == suggChannel or str(ctx.channel.id) == testChannel):
+    if(str(ctx.channel.id) == config.suggChannel or str(ctx.channel.id) == config.testChannel):
         if(not dictionary):
             await ctx.send("No suggestions... yet")
         else:
@@ -98,19 +100,19 @@ async def listSuggestions(ctx):
 # Backup method that can only be called in the TestBot server that puts the album choice poll up in case it fails for some reason
 @bot.command(name='poll')
 async def poll(ctx):
-    if(str(ctx.channel.id) == testChannel):
+    if(str(ctx.channel.id) == config.testChannel):
         await sendPoll()
 
 # Backup method that can only be called in the TestBot server that chooses the winner
 @bot.command(name='choosethewinner')
 async def choosethewinner(ctx):
-    if(str(ctx.channel.id) == testChannel):
+    if(str(ctx.channel.id) == config.testChannel):
         chooseWinner()
 
 # Deletes your suggestion for the week
 @bot.command(name='delete')
 async def delete(ctx):
-    if(str(ctx.channel.id) == testChannel):
+    if(str(ctx.channel.id) == config.testChannel):
         print(ctx.author)
         conn = sqlite3.connect('weeklyData.db')
         c = conn.cursor()
@@ -121,9 +123,9 @@ async def delete(ctx):
 
 # Chooses a winner from the album poll on the Covid Club server
 async def chooseWinner():
-    channel = bot.get_channel(839961783498571867)
+    channel = bot.get_channel(config.announcementChannel)
 
-    if(int(datetime.datetime.now().day) != getLastMonday()):
+    if(int(datetime.datetime.now().day) != getLastMeetingDay()):
         # Finds the last poll posted in the music announcements channel and gets the winner. Myself and the admins are the only ones that can post in that channel.
         async for message in channel.history(limit = 10):
             if message.author.id == 324631108731928587:
@@ -141,14 +143,13 @@ async def chooseWinner():
 
                 # Breaks ties with the random function
                 await channel.send(message.embeds[0].description.split("\n")[random.choice(index)])
-                if(int(datetime.datetime.now().day) + 7 == getLastMonday()):
+                if(int(datetime.datetime.now().day) + 7 == getLastMeetingDay()):
                     await channel.send("It's singles week! If you have a song you want everyone to hear during the meeting next week, use the suggest command with the name of the song and the artist before then!")
                 else:
-                    await channel.send("Suggestions are now open for the following week, so make sure to get them in by Saturday at 8 PM CST!")
+                    await channel.send("Suggestions are now open for the following week, so make sure to get them in by " + calendar.day_name[config.pollDay] + " at " + hourToPrintStandardTime(config.pollHour, config.pollMinute) + "!")
                 break
     else:
         print("Listing singles week songs...")
-        channel = bot.get_channel(839961783498571867)
         if(not dictionary):
             await channel.send("No suggestions :(")
         else:
@@ -168,4 +169,4 @@ async def chooseWinner():
 async def on_ready():
     runOnce()
 
-bot.run(TOKEN)
+bot.run(config.TOKEN)
