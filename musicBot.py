0f6f253c2de51d156ebf1da0e73c76a51cfc3b88
Dave Whitcome <contact@davidwhitcome.com>
2021-07-23 16:24:39 -0500

    Fix: chooseWinner cron job not running on meeting day

--- musicBot.py
+++ musicBot.py
@@ -45,7 +45,7 @@ def runOnce():
 
         scheduler = AsyncIOScheduler()
         scheduler.add_job(sendPoll, 'cron', day_of_week=config.pollDay, hour=config.pollHour, minute=config.pollMinute)
-        scheduler.add_job(chooseWinner, 'cron', day_of_week='tue', hour=config.meetingHour, minute=config.meetingMinute)
+        scheduler.add_job(chooseWinner, 'cron', day_of_week=config.meetingDay, hour=config.meetingHour, minute=config.meetingMinute)
         scheduler.start()
         print("Poll set for " + calendar.day_name[config.pollDay] + " " + hourToPrintStandardTime(config.pollHour, config.pollMinute))
         print("Meeting set for " + calendar.day_name[config.meetingDay] + " " + hourToPrintStandardTime(config.meetingHour, config.meetingMinute))
