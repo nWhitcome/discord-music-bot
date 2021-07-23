00832dc9ba97b6d674f6615b9831f19f5ef25d0e
Dave Whitcome <contact@davidwhitcome.com>
2021-07-23 16:21:08 -0500

    poll start and end time

--- config.py
+++ config.py
@@ -12,10 +12,17 @@ announcementChannel = os.getenv('ANNOUNCEMENT_CHANNEL') or 839961783498571867
 testChannel = os.getenv('TEST_CHANNEL') or 840042019971661825
 commandPrefix = '$'
 
+# Poll start date & time
+pollStartDay = os.getenv('POLL_START_DAY') or calendar.SUNDAY
+pollStartHour = os.getenv('POLL_START_HOUR') or 20
+pollStartMinute = os.getenv('POLL_START_MINUTE') or 0
+
+# Poll end date & time
+pollEndDay = os.getenv('POLL_END_DAY') or calendar.MONDAY
+pollEndHour = os.getenv('POLL_END_HOUR') or 19
+pollEndMinute = os.getenv('POLL_END_MINUTE') or 30
+
 # Meeting date & time
-pollDay = os.getenv('POLL_DAY') or calendar.SUNDAY
-pollHour = os.getenv('POLL_HOUR') or 20
-pollMinute = os.getenv('POLL_MINUTE') or 0
 meetingDay = os.getenv('MEETING_DAY') or calendar.MONDAY
 meetingHour = os.getenv('MEETING_HOUR') or 20
 meetingMinute = os.getenv('MEETING_MINUTE') or 0
